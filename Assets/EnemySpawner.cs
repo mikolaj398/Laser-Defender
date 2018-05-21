cf7f429c15c010a82fab327eb7afbb9b957bc7f4
Miki <mikolajb398@gmail.com>
2018-05-21 20:51:05 +0200

    Enemys

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float width = 10f;
+    public float heigth = 7f;
+    public float speed = 5f;
+    private bool movienRight = true;
+    float xmax;
+    float xmin;
+	void Start ()
+    {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+        Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+        xmax = rightBoundary.x;
+        xmin = leftBoundary.x;
+        foreach  (Transform child in transform)
+        {
+            GameObject enemy = Instantiate(enemyPrefab, child.transform.position, Quaternion.identity);
+            enemy.transform.parent = child;
+        }
+        
+	}
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(width, heigth,0));
+    }
+    void Update ()
+    {
+        if (movienRight) transform.position += Vector3.right * speed * Time.deltaTime;
+        else transform.position += Vector3.left * speed * Time.deltaTime;
+
+        float rightEdgeOfFormation = transform.position.x + (0.5f * width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f * width);
+        if (leftEdgeOfFormation < xmin || rightEdgeOfFormation > xmax) movienRight = !movienRight;
+    }
+}
