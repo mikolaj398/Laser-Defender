d104c436c33ca79e5d6b87e09fa5e7cf330fad50
Miki <mikolajb398@gmail.com>
2018-05-22 01:27:39 +0200

    koniec

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,15 +3,33 @@ using System.Collections;
 
 public class MusicPlayer : MonoBehaviour {
 	static MusicPlayer instance = null;
-	
-	void Start () {
+    public AudioClip startMusic;
+    public AudioClip gameMusic;
+    public AudioClip endMusic;
+
+    private AudioSource music;
+    void Start () {
 		if (instance != null && instance != this) {
 			Destroy (gameObject);
 			print ("Duplicate music player self-destructing!");
 		} else {
 			instance = this;
 			GameObject.DontDestroyOnLoad(gameObject);
+            music = GetComponent<AudioSource>();
+            music.clip = startMusic;
+            music.loop = true;
+            music.Play();
 		}
 		
 	}
+
+    private void OnLevelWasLoaded(int level)
+    {
+        music.Stop();
+        if (level==0) music.clip = startMusic;
+        else if (level == 1) music.clip = gameMusic;
+        else if (level == 2) music.clip = endMusic;
+        music.loop = true;
+        music.Play();
+    }
 }
